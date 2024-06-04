d34eb333c194792f80f980cadb0bd17335497d9d
Namrata <namrata.kesar10@gmail.com>
2024-06-04 07:40:59 +0530

    minor fix

--- Other/IP_Address.java
+++ Other/IP_Address.java
@@ -4,7 +4,9 @@ public class IP_Address
 {
     public static void main(String args[]) throws Exception
     {
-        InetAddress IP = InetAddress.getLocalHost();
+        InetAddress IP = InetAddress.getLocalHost();    //Provides host name and host address both
+        System.out.println(IP);
         System.out.println("IP of my system is := "+IP.getHostAddress());
+        System.out.println("Host name of my system is := "+IP.getHostName());
     }    
 }
